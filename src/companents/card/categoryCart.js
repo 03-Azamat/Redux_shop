20f89384453016c54d0108798eddeff3fa954b52
03-Azamat <azamatnurov10@gmail.com>
2022-06-03 09:07:07 +0600

    first commit

--- src/companents/card/categoryCart.js
+++ src/companents/card/categoryCart.js
@@ -2,15 +2,18 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import CategoryImg from "./../../img/cate.png"
 
 const CategoryCartBooks = ({el}) => {
 
     return (
 
         <div className="mx-5">
-            <Link to={'/'}
+            <Link to={`/shop-category/${el.id}`}
                   className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <img src={el.image} alt=""/>
+                <img src={el.image ? el.image : CategoryImg} alt=""
+                className="w-full h-36"
+                />
                 <div className="flex align-middle">
                     <p className="font-bold text-gray-700 dark:text-gray-400">{el.title}</p>
                     <FontAwesomeIcon
